e5856eb116d702f462912ee70c05e7bc448fc1cf
haungxi <1842196429@qq.com>
2024-01-04 08:14:51 +0800

    🐞 fix(密码不再同步到github): 密码不再同步到github

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { User } from './user/entities/user.entity';
+import { mysqlConfig } from './configuration'
 import { UserModule } from './user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 // import { createClient } from 'redis';
@@ -10,22 +10,7 @@ import { JwtModule } from '@nestjs/jwt';
 @Module({
   imports: [
     UserModule,
-    TypeOrmModule.forRoot({
-      type: "mysql",
-      host: "sh-cynosdbmysql-grp-rrgrxyr4.sql.tencentcdb.com",
-      port: 20020,
-      username: "root",
-      password: "Huwenting520.520",
-      database: "demo",
-      synchronize: true,
-      logging: true,
-      entities: [User],
-      poolSize: 10,
-      connectorPackage: 'mysql2',
-      extra: {
-        authPlugin: 'sha256_password',
-      }
-    }),
+    TypeOrmModule.forRoot(mysqlConfig as TypeOrmModuleOptions),
     JwtModule.register({
       global: true,
       secret: 'huangxi',
